f9d1780d15561eefaba7374f8762b9c7bc18f230
Sujayraj Boregouda <sujayraj.gouda@gmail.com>
2024-04-19 00:08:50 +0530

    Indentation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,55 +18,55 @@ export default function Header() {
                   <Logo />
               </Link>
             </div>
-
+            
             <div className="header-menu">
-                          <ul>
-                              <li>
-                                  <NavLink
-                                  to="/"
-                                      className={({isActive}) =>
-                                        `${isActive ? "secondary" : "primary-text-black"}`
-                                      }
-                                  >
-                                      <span className='secondary-fs'>Home</span>
-                                      <span className='icon'>
-                                        <svg width="10" height="10" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                          <path d="M1.47108 0.527293C1.21073 0.266944 0.788619 0.266944 0.52827 0.527293C0.26792 0.787643 0.267921 1.20975 0.52827 1.4701L4.52827 5.4701C4.78862 5.73045 5.21073 5.73045 5.47108 5.4701L9.47108 1.4701C9.73143 1.20975 9.73143 0.787641 9.47108 0.527292C9.21073 0.266942 8.78862 0.266942 8.52827 0.527292L4.99967 4.05589L1.47108 0.527293Z" fill="#212121"/>
-                                        </svg>
-                                      </span>
-                                  </NavLink>
-                              </li>
-                              <li>
-                                  <NavLink
-                                  to="/lettings"
-                                      className={({isActive}) =>
-                                        `${isActive ? "secondary" : "primary-text-black"}`
-                                      }
-                                  >
-                                      <span className='secondary-fs'>Lettings</span>
-                                      <span className='icon'>
-                                        <svg width="10" height="10" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                          <path d="M1.47108 0.527293C1.21073 0.266944 0.788619 0.266944 0.52827 0.527293C0.26792 0.787643 0.267921 1.20975 0.52827 1.4701L4.52827 5.4701C4.78862 5.73045 5.21073 5.73045 5.47108 5.4701L9.47108 1.4701C9.73143 1.20975 9.73143 0.787641 9.47108 0.527292C9.21073 0.266942 8.78862 0.266942 8.52827 0.527292L4.99967 4.05589L1.47108 0.527293Z" fill="#212121"/>
-                                        </svg>
-                                      </span>
-                                  </NavLink>
-                              </li>
-                              <li>
-                                  <NavLink
-                                  to="/rent"
-                                      className={({isActive}) =>
-                                        `${isActive ? "secondary" : "primary-text-black"}`
-                                      }
-                                  >
-                                      <span className='secondary-fs'>Rent</span>
-                                      <span className='icon'>
-                                        <svg width="10" height="10" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                          <path d="M1.47108 0.527293C1.21073 0.266944 0.788619 0.266944 0.52827 0.527293C0.26792 0.787643 0.267921 1.20975 0.52827 1.4701L4.52827 5.4701C4.78862 5.73045 5.21073 5.73045 5.47108 5.4701L9.47108 1.4701C9.73143 1.20975 9.73143 0.787641 9.47108 0.527292C9.21073 0.266942 8.78862 0.266942 8.52827 0.527292L4.99967 4.05589L1.47108 0.527293Z" fill="#212121"/>
-                                        </svg>
-                                      </span>
-                                  </NavLink>
-                              </li>
-                          </ul>
+                <ul>
+                    <li>
+                        <NavLink
+                        to="/"
+                            className={({isActive}) =>
+                              `${isActive ? "secondary" : "primary-text-black"}`
+                            }
+                        >
+                            <span className='secondary-fs'>Home</span>
+                            <span className='icon'>
+                              <svg width="10" height="10" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1.47108 0.527293C1.21073 0.266944 0.788619 0.266944 0.52827 0.527293C0.26792 0.787643 0.267921 1.20975 0.52827 1.4701L4.52827 5.4701C4.78862 5.73045 5.21073 5.73045 5.47108 5.4701L9.47108 1.4701C9.73143 1.20975 9.73143 0.787641 9.47108 0.527292C9.21073 0.266942 8.78862 0.266942 8.52827 0.527292L4.99967 4.05589L1.47108 0.527293Z" fill="#212121"/>
+                              </svg>
+                            </span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                        to="/lettings"
+                            className={({isActive}) =>
+                              `${isActive ? "secondary" : "primary-text-black"}`
+                            }
+                        >
+                            <span className='secondary-fs'>Lettings</span>
+                            <span className='icon'>
+                              <svg width="10" height="10" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1.47108 0.527293C1.21073 0.266944 0.788619 0.266944 0.52827 0.527293C0.26792 0.787643 0.267921 1.20975 0.52827 1.4701L4.52827 5.4701C4.78862 5.73045 5.21073 5.73045 5.47108 5.4701L9.47108 1.4701C9.73143 1.20975 9.73143 0.787641 9.47108 0.527292C9.21073 0.266942 8.78862 0.266942 8.52827 0.527292L4.99967 4.05589L1.47108 0.527293Z" fill="#212121"/>
+                              </svg>
+                            </span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                        to="/rent"
+                            className={({isActive}) =>
+                              `${isActive ? "secondary" : "primary-text-black"}`
+                            }
+                        >
+                            <span className='secondary-fs'>Rent</span>
+                            <span className='icon'>
+                              <svg width="10" height="10" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1.47108 0.527293C1.21073 0.266944 0.788619 0.266944 0.52827 0.527293C0.26792 0.787643 0.267921 1.20975 0.52827 1.4701L4.52827 5.4701C4.78862 5.73045 5.21073 5.73045 5.47108 5.4701L9.47108 1.4701C9.73143 1.20975 9.73143 0.787641 9.47108 0.527292C9.21073 0.266942 8.78862 0.266942 8.52827 0.527292L4.99967 4.05589L1.47108 0.527293Z" fill="#212121"/>
+                              </svg>
+                              </span>
+                        </NavLink>
+                    </li>
+                  </ul>
             </div>
 
             <div className='header-icons'>
